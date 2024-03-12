1f69d0a877f3385d9e37e0cf79e34431e097bdcc
Hleb Nikalayenka <s28311@pjwstk.edu.pl>
2024-03-12 10:55:02 +0100

    Change variable name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,9 @@ for (int i = 0; i < 10; i++)
 static int calculateAverage(int[] ar)
 {
     int total = 0;
-    foreach (int n in ar)
+    foreach (int v in ar)
     {
-        total += n;
+        total += v;
     }
 
     return total / ar.Length;
